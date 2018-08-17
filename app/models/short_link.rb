2184c1f257f4d097e3cb884bac05e0e20bffa625
Tom Reznick <thomas.reznick@gmail.com>
2018-08-16 22:14:25 -0400

    Wire up friendly_id generation

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,2 +1,9 @@
 class ShortLink < ApplicationRecord
+  before_save :generate_friendly_id, on: :create
+
+  private
+
+  def generate_friendly_id
+    self.friendly_id = SecureRandom.hex(4)
+  end
 end
--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "friendly_id generation" do
+    context "given a valid url" do
+      let(:short_link) { ShortLink.new(url: "www.google.com") }
+
+      it "returns a short code" do
+        short_link.save
+        expect(short_link.friendly_id).to match(/[A-Za-z0-9]{8}/)
+      end
+    end
+  end
 end
