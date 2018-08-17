971c5e3209d6e5f286d7f4e397927a8d8d34cda7
Tom Reznick <thomas.reznick@gmail.com>
2018-08-16 23:34:19 -0400

    Appropriately Redirect from short_urls

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -1,6 +1,7 @@
 class ShortLinksController < ApplicationController
   before_action :build_short_link, only: [:index]
   before_action :load_short_links, only: [:index]
+  before_action :find_short_link, only: [:show]
 
   def index
   end
@@ -17,7 +18,19 @@ class ShortLinksController < ApplicationController
     end
   end
 
+  def show
+    if @short_link
+      redirect_to @short_link.url
+    else
+      redirect_to action: :index
+    end
+  end
+
   private
+
+  def find_short_link
+    @short_link = ShortLink.find_by(friendly_id: params[:friendly_id])
+  end
 
   def build_short_link
     @short_link = ShortLink.new
